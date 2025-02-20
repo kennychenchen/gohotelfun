8a11f1604fa30696a173a4f030610e9935ac0fbf
kennychenchen <42375711+kennychenchen@users.noreply.github.com>
2025-02-20 14:09:34 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
-const now = new Date();
+function updateTime() {
+    const now = new Date();
     const timeString = now.toLocaleTimeString();
     document.getElementById('current-time').textContent = timeString;
     // 获取当前年份
@@ -8,3 +9,8 @@ const now = new Date();
     if (copyrightElement) {
         copyrightElement.textContent = `© ${currentYear} gohotel.fun`;
     }
+}
+// 每秒更新一次时间
+setInterval(updateTime, 1000);
+// 页面加载时立即更新一次时间
+updateTime();
